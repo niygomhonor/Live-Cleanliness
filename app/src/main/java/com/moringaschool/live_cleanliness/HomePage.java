b655f479076becbf4e6878308132ebaac4f97422
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-11-21 00:00:43 +0200

    Upload picture

--- app/src/main/java/com/moringaschool/live_cleanliness/HomePage.java
+++ app/src/main/java/com/moringaschool/live_cleanliness/HomePage.java
@@ -1,6 +1,7 @@
 package com.moringaschool.live_cleanliness;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -8,8 +9,20 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.app.FragmentManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -18,12 +31,16 @@ import com.moringaschool.live_cleanliness.fragments.InstallationFragment;
 import com.moringaschool.live_cleanliness.fragments.PaintingFragment;
 import com.moringaschool.live_cleanliness.fragments.TransportFragment;
 import com.moringaschool.live_cleanliness.fragments.WellnessFragment;
+import com.squareup.picasso.Picasso;
 
 
 public class HomePage extends AppCompatActivity  implements  NavigationView.OnNavigationItemSelectedListener{
 private DrawerLayout drawer;
 private ActionBarDrawerToggle toggle;
 NavigationView navigationView;
+    public Uri imgUri;
+    private static final int REQUEST_IMAGE_CAPTURE = 111;
+    ImageView images;
 
 
     @Override
@@ -35,7 +52,7 @@ NavigationView navigationView;
         toggle=new ActionBarDrawerToggle(this,drawer,R.string.open,R.string.close);
 
         navigationView=findViewById(R.id.nav_view);
-
+        images=findViewById(R.id.imageView);
 
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -49,15 +66,47 @@ if (savedInstanceState==null) {
     navigationView.setCheckedItem(R.id.nav_Installation);
 }
 
+
+images.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        FileChooser();
+        Toast.makeText(HomePage.this, "He loves you", Toast.LENGTH_SHORT).show();
+    }
+});
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if( toggle.onOptionsItemSelected(item)) {
+        if (toggle.onOptionsItemSelected(item)){
+
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void FileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+//        startActivityForResult(intent.createChooser(intent,"Select image"),CHOOSE_IMAGE);
+
+        startActivityForResult(intent, 1);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imgUri = data.getData();
+            images.setImageURI(imgUri);
+            Picasso.get().load(imgUri).into(images);
+        }
+    }
+
+
+
 
     @Override
     public void onBackPressed() {
@@ -106,7 +155,11 @@ if (savedInstanceState==null) {
 
     drawer.closeDrawer(GravityCompat.START);
         return true;
+
+
+
     }
+
 
 
     }
